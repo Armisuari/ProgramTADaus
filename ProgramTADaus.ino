7d0fb087b8ca868e148e70e5be93eff9908ded92
Arief Nur Misuari <arief.nur@efishery.com>
2024-05-13 02:48:48 +0800

    style(main): move kondisiSaldo enum object to global declarration fix(main): fix getsaldo aritmatik feat(main): add methode to switch lcd display

--- ProgramTADaus.ino
+++ ProgramTADaus.ino
@@ -7,19 +7,22 @@ LiquidCrystal_I2C lcd(0x26, 16, 2);
 PzemInterface pzem;
 PLCInterface plc;
 
-unsigned int saldoToken = 200000;
+unsigned int saldoToken = 6000;
 
 PzemData_t data;
 float currentSaldo;
 
-typedef enum {
+typedef enum
+{
   AMAN,
   HAMPIR_HABIS,
   HAMPIR_HABIS_SEKALI,
   HABIS
 } KondisiSaldo;
+KondisiSaldo kondisiSaldo;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   // Serial.begin(115200);
 
@@ -29,64 +32,171 @@ void setup() {
   lcd.print("Sistem Manajemen");
   lcd.setCursor(0, 1);
   lcd.print("Pembatas Energi");
-  delay(1000);
+  delay(2000);
 
   pzem.begin();
   plc.begin();
 
-  currentSaldo = saldoToken / 1444;
+  currentSaldo = float(saldoToken) / 1444.0 * 1000; // wh unit
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
-  if (currentSaldo < 0) currentSaldo = 0;
+  if (currentSaldo < 0.0)
+    currentSaldo = 0;
 
   static unsigned long prevMillPzem;
-  if (millis() - prevMillPzem >= 500U) {
+  if (millis() - prevMillPzem >= 3000UL)
+  {
     prevMillPzem = millis();
-    readDisplay();
+
+    static bool _switch;
+    if (!_switch)
+    {
+      readDisplay();
+      _switch = true;
+    }
+    else
+    {
+      readDisplayAlt();
+      _switch = !_switch;
+    }
   }
 
   static unsigned long prevMillSaldo;
-  if (millis() - prevMillSaldo >= 6000U) {
+  if (millis() - prevMillSaldo >= 60000U)
+  {
     prevMillSaldo = millis();
     currentSaldo = getSaldo();
   }
 
-  KondisiSaldo kondiSaldo;
-  if (currentSaldo > 5.0)
-  {
-    kondisiSaldo
-  }
-
-  plc.update();
+  // Check and update saldo condition
+  kondisiSaldo = updateSaldoCondition(currentSaldo);
+  lcd.setCursor(15, 0);
+  lcd.print(kondisiSaldo);
 
   static int i;
   static unsigned long prevMillPlc;
-  if (millis() - prevMillPlc >= 1000U) {
+  if (millis() - prevMillPlc >= 1000U)
+  {
     prevMillPlc = millis();
-    if (i <= 2) {
-      plc.regs[0] = data.value[0 + i];
-      i++;
-    } else {
-      i = 0;
+    plc.update(0, 220);
+    plc.update(1, 20);
+    plc.update(2, kondisiSaldo);
+
+    //   // static bool state;
+    //   // if (!state)
+    //   // {
+    //   //   state = true;
+    //   // }
+    //   // else
+    //   // {
+    //   //   state = false;
+    //   // }
+
+    //   // plc.update_coil(0, state);
+    //   // Serial.printf("state: %d\n", state);
+  }
+
+  float getSaldo()
+  {
+    return currentSaldo - (data.value[1] / 60);
+  }
+
+  void readDisplay()
+  {
+    static float previousPower;
+    static KondisiSaldo previousSaldo;
+
+    // Read voltage and power
+    pzem.readVoltage(0, data);
+    pzem.readPower(0, data);
+    // data.value[1] = 1000;
+
+    if (currentSaldo <= 0.0)
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(" Token Listrik ");
+      lcd.setCursor(0, 1);
+      lcd.print("     Habis     ");
+
+      // Serial.println("Token Listrik Habis !!!");
+    }
+    else
+    {
+
+      // Check if the values have changed before updating the LCD
+      if (data.value[1] != previousPower)
+      {
+        // Update previous values
+        previousPower = data.value[1];
+
+        // Clear the LCD and update the display
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.printf("Daya: %.1f W", previousPower);
+
+        lcd.setCursor(0, 1);
+        if (currentSaldo / 1000 < 0.001)
+        {
+          lcd.printf("Saldo: %.3f Wh", currentSaldo);
+        }
+        else
+        {
+          lcd.printf("Saldo: %.3f kWh", currentSaldo / 1000);
+        }
+
+        // Serial.printf("saldo: %.9f kWh | daya: %.3f W\n", currentSaldo / 1000, previousPower);
+      }
     }
   }
-}
 
-float getSaldo() {
-  return currentSaldo - (data.value[1] / 60);
-}
+  void readDisplayAlt()
+  {
+    // static float previousVoltage;
+    // static float previousCurrent;
+    // static float previousPF;
 
-void readDisplay() {
-  pzem.readVoltage(0, data);
-  // pzem.readPower(0, data);
+    // if (data.value[0] != previousVoltage || data.value[2] != previousCurrent || data.value[4] != previousPF) {
+    //   previousVoltage = data.value[0];
+    //   previousCurrent = data.value[2];
+    //   previousPF = data.value[4];
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.printf("Daya: %.f W", data.value[1]);
+    pzem.readVoltage(0, data);
+    pzem.readCurrent(0, data);
+    pzem.readPF(0, data);
 
-  lcd.setCursor(0, 1);
-  lcd.printf("Saldo: %.1f kWh", currentSaldo);
-}
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.printf("v:%.f  pf:%.2f", data.value[0], data.value[4]);
+    // lcd.printf("v: %.1f  i: %.1f", previousVoltage, previousCurrent);
+
+    lcd.setCursor(0, 1);
+    lcd.printf("i:%.2f", data.value[2]);
+    // lcd.printf("pf: %.1f", previousPF);
+    // }
+  }
+
+  // Function to update saldo condition based on currentSaldo value
+  KondisiSaldo updateSaldoCondition(float saldo)
+  {
+    if (saldo >= 5000)
+    {
+      return AMAN;
+    }
+    else if (saldo > 2000)
+    {
+      return HAMPIR_HABIS;
+    }
+    else if (saldo > 0)
+    {
+      return HAMPIR_HABIS_SEKALI;
+    }
+    else
+    {
+      return HABIS;
+    }
+  }
